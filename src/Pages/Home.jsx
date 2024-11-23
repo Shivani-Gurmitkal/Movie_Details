9f4c08ada48b346c1a33d24ee257f0444d5f4fdb
Shivani-Gurmitkal <shivanigurmitkal@gmail.com>
2024-11-23 16:50:46 +0530

    sub-heading change in homepage

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -10,7 +10,7 @@ function Home() {
 
     <div className="text-center w-[680px] mx-auto mt-40">
       <h1 className="text-white text-4xl w-[640px] ml-5 font-bold">Your Gateway to the World of Movies</h1>
-      <p className="text-neutral-200 text-sm mt-4">Discover everything about your favorite films, from cast and crew to reviews and trivia</p>
+      <p className="text-neutral-200 text-sm mt-4">Discover everything about your favorite films, from cast and crew to reviews</p>
       <Link to="/movies"><button className='mt-5 bg-yellow-500 text-white p-4 rounded-md font-bold text-xl'>Explore Movies</button></Link>
     </div>
 
